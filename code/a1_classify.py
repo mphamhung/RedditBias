f64d4bfe6c5870e7862ac2f6fa3728d67c524305
mphamhung <hungmic2@teach.cs.toronto.edu>
2019-02-09 00:58:43 -0500

    late fridays

--- code/a1_classify.py
+++ code/a1_classify.py
@@ -14,6 +14,10 @@ from sklearn.metrics import confusion_matrix
 import csv
 from sklearn.utils import resample
 
+from sklearn.model_selection import KFold
+from scipy import stats
+
+skipTraining = False
 def accuracy( C ):
     ''' Compute accuracy given Numpy array confusion matrix C. Returns a floating point value '''
     correct = 0
@@ -46,8 +50,8 @@ def precision( C ):
     
 
 classifiers = [0,1,2,3,4]
-classifiers[0] = SVC(kernel = 'linear' ,verbose = True, max_iter = 1000)
-classifiers[1] = SVC(gamma = 2,verbose = True, max_iter = 1000)
+classifiers[0] = SVC(kernel = 'linear' ,max_iter = 1000)
+classifiers[1] = SVC(gamma = 2, max_iter = 1000)
 classifiers[2] = RandomForestClassifier(max_depth = 5, n_estimators = 10,verbose = True)
 classifiers[3] = MLPClassifier(alpha = 0.05,verbose = True)
 classifiers[4] = AdaBoostClassifier()
@@ -71,9 +75,11 @@ def class31(filename):
     print("splitting data")
     X_train, X_test, y_train, y_test = train_test_split([row[:-1] for row in data], [row[-1] for row in data], train_size = 0.8, random_state = 0)
     
+    if skipTraining:
+        return (X_train, X_test, y_train, y_test, 4)
     with open('a1_3.1.csv', 'w', newline = '') as csvfile:
         cwriter = csv.writer(csvfile)        
-        cwriter.writerow(['classifier id', 'accuracy', 'recall: 0' , 'recall: 1' , 'recall: 2' , 'recall: 3' , 'precision: 0','precision: 1','precision: 2','precision: 3', 'C' ])
+        #cwriter.writerow(['classifier id', 'accuracy', 'recall: 0' , 'recall: 1' , 'recall: 2' , 'recall: 3' , 'precision: 0','precision: 1','precision: 2','precision: 3', 'C' ])
         
         best = 0
         for num, key in enumerate(keys):
@@ -88,10 +94,10 @@ def class31(filename):
             
             if accuracy(C) > best:
                 iBest = num
-                best = sum(perf)
+                best = accuracy(C) 
                 
-    print("The best classifer was: " + keys[iBest])
-    return (X_train, X_test, y_train, y_test,iBest)
+    print("The best classifer was: " + keys[iBest] + str(iBest))
+    return (X_train, X_test, y_train, y_test, iBest)
 
 
 def class32(X_train, X_test, y_train, y_test,iBest):
@@ -111,11 +117,10 @@ def class32(X_train, X_test, y_train, y_test,iBest):
     sizes = [1000, 5000,10000,15000,20000]
     
     bestClassifier = classifiers[iBest]
-    np.random.seed(0)
     
     with open('a1_3.2.csv', 'w', newline = '') as csvfile:
         cwriter = csv.writer(csvfile)        
-        cwriter.writerow(sizes)
+        #cwriter.writerow(sizes)
         
         accuracies = []
         for size in sizes:
@@ -127,9 +132,10 @@ def class32(X_train, X_test, y_train, y_test,iBest):
             if size == 1000:
                 X_1k, y_1k = sampled_x_train, sampled_y_train
             
+        cwriter.writerow(accuracies)
     return (X_1k, y_1k)
     
-def class33(X_train, X_test, y_train, y_test, i, X_1k, y_1k):
+def class33(X_train, X_test, y_train, y_test, iBest, X_1k, y_1k):
     ''' This function performs experiment 3.3
     
     Parameters:
@@ -145,26 +151,96 @@ def class33(X_train, X_test, y_train, y_test, i, X_1k, y_1k):
     numfeats = [5,10,20,30,40,50]
     with open('a1_3.3.csv', 'w', newline = '') as csvfile:
         cwriter = csv.writer(csvfile)        
-        #cwriter.writerow(sizes)
-        
-        for k in numfeats:
-            selector = SelectKBest(score_func=chi2, k=k)
+
+        #cwriter.writerow(['32k training set'])
+        #cwriter.writerow(['num Features', 'p-values'])
+        print('32k training set')
+        sum32 = 0
+        for n in numfeats:
+            print("{} features".format(n))
+            selector = SelectKBest(k=n)
             X_new = selector.fit_transform(X_train, y_train)
             pp = selector.pvalues_
-            print(X_new)
-            print(pp)
+
+            featureIndices = selector.get_support(indices=True)
+            print(featureIndices)
+            cwriter.writerow([n] + [pp[i] for i in featureIndices])
+            sum32 += sum([pp[i] for i in featureIndices])
+
+        print('1k training set')
+        sum1 = 0
+        for n in numfeats:
+            print("{} features".format(n))
+            selector = SelectKBest(k=n)
+            X_new = selector.fit_transform(X_train, y_train)
+            pp = selector.pvalues_
+            featureIndices = selector.get_support(indices=True)
+            print(featureIndices)
+            sum1 += sum([pp[i] for i in featureIndices])
+
+        bestClassifier = classifiers[iBest]
+        selector = SelectKBest(k=5)
+        X_new_32k = selector.fit_transform(X_train, y_train)
+        X_new_1k = selector.fit_transform(X_1k, y_1k)
+        featureIndices = selector.get_support(indices=True).tolist()
+        X_new_test = [[row[i] for i in featureIndices] for row in X_test]
+
+        bestClassifier.fit(X_new_32k , y_train)
+        y_pred = bestClassifier.predict(X_new_test)
+        C = confusion_matrix(y_test, y_pred)
+        acc_32k = accuracy(C)
     
+        bestClassifier.fit(X_new_1k , y_1k)
+        y_pred = bestClassifier.predict(X_new_test)
+        C = confusion_matrix(y_test, y_pred)
+        acc_1k = accuracy(C)
+        
+        cwriter.writerow([acc_1k,acc_32k])
+        print(sum32, sum1)
     
-def class34( filename, i ):
+def class34( filename, iBest ):
     ''' This function performs experiment 3.4
     
     Parameters
        filename : string, the name of the npz file from Task 2
        i: int, the index of the supposed best classifier (from task 3.1)  
         '''
-    print('TODO Section 3.4')
-    
-    
+    kf = KFold(n_splits = 5, shuffle = True, random_state = 0)
+    data = np.load(filename)
+    data = data[data.files[0]]
+    X = [row[:-1] for row in data]
+    y = [row[-1] for row in data]
+
+    a = [[] for i in range(len(classifiers))]
+    with open('a1_3.4.csv', 'w', newline='') as csvfile:
+        cwriter = csv.writer(csvfile)
+
+        for train_index, test_index in kf.split(X, y):
+            X_train = [X[i] for i in train_index]
+            X_test = [X[i] for i in test_index]
+            y_train = [y[i] for i in train_index]
+            y_test = [y[i] for i in test_index]
+
+            accuracies = []
+            for num, key in enumerate(keys):
+                print("Fitting " + key)
+                classifiers[num].fit(X_train, y_train)
+                print("Predicting " + key)
+                y_pred = classifiers[num].predict(X_test)
+                C = confusion_matrix(y_test, y_pred)
+                accuracies.append(accuracy(C))
+                a[num].append(accuracy(C))
+            cwriter.writerow(accuracies)
+        p = []
+        for num, key in enumerate(keys):
+            if num == iBest:
+                continue
+            S = stats.ttest_rel(a[num], a[iBest])
+            p.append(S)
+
+        cwriter.writerow(p)
+
+
 if __name__ == "__main__":
     
     parser = argparse.ArgumentParser(description='aaa')
@@ -174,5 +250,6 @@ if __name__ == "__main__":
 
     # TODO : complete each classification experiment, in sequence.
     X_train, X_test, y_train, y_test,iBest = class31(args.input)
-    class32(X_train, X_test, y_train, y_test,iBest)
-    
+    X_1k, y_1k = class32(X_train, X_test, y_train, y_test,iBest)
+    class33( X_train, X_test, y_train, y_test,iBest, X_1k, y_1k)
+    class34(args.input, iBest)
